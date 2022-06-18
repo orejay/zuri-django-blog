8015edb0537415cd71ff13bc9840a6e34f651d98
Ore Jaiyesimi <orejay2017@gmail.com>
2022-06-18 22:55:04 +0100

    migrated

--- I4G008653VSD/blog/models.py
+++ I4G008653VSD/blog/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, default=1)
     Created_date = models.DateTimeField()
     Published_date = models.DateTimeField()
